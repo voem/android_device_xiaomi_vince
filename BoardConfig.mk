f2ea00cc033990b168eb5477184bfb7e4781651d
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-01-20 12:55:04 +0700

    vince: Update DT2W node

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -183,7 +183,7 @@ TARGET_PER_MGR_ENABLED := true
 
 # Power
 TARGET_HAS_NO_WLAN_STATS := true
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
+TARGET_TAP_TO_WAKE_NODE := "/sys/devices/soc/78b7000.i2c/i2c-3/3-0020/input/input1/wake_gesture"
 TARGET_USES_INTERACTION_BOOST := true
 
 # Qualcomm
