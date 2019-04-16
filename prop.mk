103da3dcf0125ddadc53647705a1ae75f65eff79
Rama Bondan Prakoso <ramanarubp@gmail.com>
2019-04-16 09:09:08 +0000

    vince: Reorder properties like CAF

--- prop.mk
+++ prop.mk
@@ -1,257 +1,86 @@
-#
-# system.prop for vince
-#
-
-# ART
+# vendor prop from tissot
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dex2oat-filter=speed \
-dalvik.vm.image-dex2oat-filter=speed
-
-# Audio
-PRODUCT_PROPERTY_OVERRIDES += \
+ro.config.media_vol_default=10 \
+dalvik.vm.heapminfree=4m \
+dalvik.vm.heapstartsize=16m \
+dalvik.vm.dex2oat-threads=6 \
+dalvik.vm.heapgrowthlimit=192m \
+dalvik.vm.heapsize=512m \
+dalvik.vm.heaptargetutilization=0.75 \
+dalvik.vm.heapmaxfree=8m \
+vendor.hw.fm.init=0 \
+persist.radio.multisim.config=dsds \
+persist.vendor.qcomsysd.enabled=1 \
+keyguard.no_require_sim=true \
+ro.com.android.dataroaming=false \
+ro.vendor.extension_library=libqti-perfd-client.so \
+persist.vendor.radio.apm_sim_not_pwdn=1 \
+persist.vendor.radio.sib16_support=1 \
+persist.vendor.radio.custom_ecc=1 \
+persist.vendor.radio.rat_on=combine \
+persist.backup.ntpServer=0.pool.ntp.org \
+sys.vendor.shutdown.waittime=500 \
+ro.frp.pst=/dev/block/bootdevice/by-name/config \
+ro.opengles.version=196610 \
 af.fast_track_multiplier=2 \
-audio.deep_buffer.media=true \
+vendor.audio_hal.period_size=192 \
+ro.vendor.audio.sdk.fluencetype=fluence \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+persist.vendor.audio.fluence.speaker=true \
 audio.offload.disable=true \
+vendor.audio.tunnel.encode=false \
+vendor.audio.offload.buffer.size.kb=64 \
 audio.offload.min.duration.secs=30 \
 audio.offload.video=true \
-persist.audio.dirac.speaker=true \
-persist.vendor.audio.fluence.speaker=true \
-persist.vendor.audio.fluence.voicecall=true \
-persist.vendor.audio.fluence.voicerec=true \
-persist.vendor.audio.hw.binder.size_kbyte=1024 \
-persist.vendor.btstack.enable.splita2dp=false \
-ro.config.media_vol_steps=25 \
-ro.config.vc_call_vol_steps=7 \
-ro.vendor.audio.sdk.fluencetype=fluence \
-ro.vendor.audio.sdk.ssr=false \
-vendor.audio.flac.sw.decoder.24bit=true \
-vendor.audio.offload.buffer.size.kb=64 \
-vendor.audio.offload.gapless.enabled=true \
+vendor.audio.offload.track.enable=true
+audio.deep_buffer.media=true \
+vendor.voice.path.for.pcm.voip=true \
 vendor.audio.offload.multiaac.enable=true \
+vendor.audio.dolby.ds2.enabled=false \
+vendor.audio.dolby.ds2.hardbypass=false \
 vendor.audio.offload.multiple.enabled=false \
-vendor.audio.offload.track.enable=true \
+vendor.audio.offload.passthrough=false \
+ro.vendor.audio.sdk.ssr=false \
+vendor.audio.offload.gapless.enabled=true \
+vendor.audio.safx.pbe.enabled=true \
 vendor.audio.parser.ip.buffer.size=262144 \
 vendor.audio.playback.mch.downsample=true \
-vendor.audio.pp.asphere.enabled=false \
-vendor.audio.safx.pbe.enabled=true \
-vendor.audio.tunnel.encode=false \
 vendor.audio.use.sw.alac.decoder=true \
 vendor.audio.use.sw.ape.decoder=true \
-vendor.audio_hal.period_size=192 \
-vendor.voice.conc.fallbackpath=deep-buffer \
-vendor.voice.path.for.pcm.voip=true \
+vendor.audio.pp.asphere.enabled=false \
 vendor.voice.playback.conc.disabled=true \
 vendor.voice.record.conc.disabled=false \
-vendor.voice.voip.conc.disabled=true
-
-# Bluetooth
-PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.hfp.client=1 \
-vendor.qcom.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
-
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.camera.display.lmax=1280x720 \
-persist.vendor.camera.display.umax=1920x1080 \
-camera.hal1.packagelist=com.skype.raider,com.google.android.talk \
-camera.lowpower.record.enable=1 \
-media.camera.ts.monotonic=1 \
-persist.camera.CDS=off \
-persist.camera.video.CDS=off \
-persist.camera.dual.camera=0 \
-persist.camera.gyro.disable=0 \
-persist.camera.isp.clock.optmz=0 \
-persist.camera.stats.test=5 \
-persist.vendor.qti.telephony.vt_cam_interface=1 \
-sdm.idle_time=1000 \
-vendor.camera.aux.packagelist2=com.android.systemui \
-vendor.camera.aux.packagelist=org.codeaurora.snapcam \
-vidc.enc.dcvs.extra-buff-count=2
+vendor.voice.voip.conc.disabled=true \
+vendor.voice.conc.fallbackpath=deep-buffer \
+persist.vendor.audio.speaker.prot.enable=false \
+vendor.audio.hw.aac.encoder=true \
+vendor.audio.flac.sw.decoder.24bit=true \
+ro.af.client_heap_size_kbyte=7168 \
+persist.vendor.audio.hw.binder.size_kbyte=1024 \
+rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+vendor.rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+drm.service.enabled=true
 
 # Camera HAL 3
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.camera.HAL3.enabled=1
 
-# Cne
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.cne.feature=1
-
-# Coresight
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.coresight.config=stm-events
-
-# Display
-PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
-debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
-debug.sf.recomputecrop=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
-ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.sf.lcd_density=440 \
-ro.vendor.display.cabl=2 \
-sdm.debug.disable_skip_validate=1 \
-sys.paper_mode_max_level=32 \
-vendor.display.enable_default_color_mode=1 \
-vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1
-
-# DRM
-PRODUCT_PROPERTY_OVERRIDES += \
-drm.service.enabled=true
-
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.qfp=false
-
-# Fm
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.fm.transmitter=false \
-vendor.hw.fm.init=0
-
-# Frp
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.frp.pst=/dev/block/bootdevice/by-name/config
-
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-av.debug.disable.pers.cache=1 \
-media.aac_51_output_enabled=true \
-media.msm8956hw=0 \
-media.stagefright.audio.sink=280 \
-mm.enable.smoothstreaming=true \
-mmp.enable.3g2=true \
-vendor.audio.hw.aac.encoder=true \
-vendor.mm.enable.qcom_parser=1048575 \
-vendor.vidc.dec.downscalar_height=1088 \
-vendor.vidc.dec.downscalar_width=1920 \
-vendor.vidc.disable.split.mode=1 \
-vendor.vidc.enc.disable.pq=true \
-vendor.vidc.enc.disable_bframes=1 \
-vendor.video.disable.ubwc=1
+ro.config.vc_call_vol_steps=7 \
+ro.config.media_vol_steps=25
 
-# Perf
+# Dpi
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.sys.fw.dex2oat_thread_count=8 \
-ro.vendor.extension_library=libqti-perfd-client.so \
-ro.vendor.qti.sys.fw.bservice_enable=true
+ro.sf.lcd_density=440
 
-# Netmgrd
+# ART
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-persist.vendor.data.mode=concurrent
+dalvik.vm.dex2oat-filter=speed \
+dalvik.vm.image-dex2oat-filter=speed \
+ro.sys.fw.dex2oat_thread_count=8
 
-# Nitz
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.rild.nitz_plmn="" \
-persist.rild.nitz_long_ons_0="" \
-persist.rild.nitz_long_ons_1="" \
-persist.rild.nitz_long_ons_2="" \
-persist.rild.nitz_long_ons_3="" \
-persist.rild.nitz_short_ons_0="" \
-persist.rild.nitz_short_ons_1="" \
-persist.rild.nitz_short_ons_2="" \
-persist.rild.nitz_short_ons_3=""
-
-# Radio
-PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
-persist.data.iwlan.enable=true \
-persist.dbg.ims_volte_enable=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.dbg.wfc_avail_ovr=0 \
-persist.radio.calls.on.ims=0 \
-persist.radio.csvt.enabled=false \
-persist.radio.jbims=0 \
-persist.radio.mt_sms_ack=20 \
-persist.radio.multisim.config=dsds \
-persist.radio.videopause.mode=1 \
-persist.vendor.radio.apm_sim_not_pwdn=1 \
-persist.vendor.radio.custom_ecc=1 \
-persist.vendor.radio.hw_mbn_update=0 \
-persist.vendor.radio.rat_on=combine \
-persist.vendor.radio.sw_mbn_update=0 \
-persist.vendor.radio.sib16_support=1 \
-ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=22,20 \
-service.qti.ims.enabled=1 \
-telephony.lteOnCdmaDevice=1
-
-# Time Services
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
-
-# Tcp
-PRODUCT_PROPERTY_OVERRIDES += \
-net.tcp.2g_init_rwnd=10
-
-# UI
-PRODUCT_PROPERTY_OVERRIDES += \
-sys.use_fifo_ui=1
-
-# Usb
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config.extra=none
-
-# Wifi
-PRODUCT_PROPERTY_OVERRIDES += \
-wifi.interface=wlan0
-
-# Wifi-Display
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.wfd.enable=1 \
-persist.sys.wfd.virtual=0
-
+# XenonHD maintainer
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.xenonhd.maintainer=rama982
-
-# Unsorted properties
-PRODUCT_PROPERTY_OVERRIDES += \
-keyguard.no_require_sim=true \
-persist.backup.ntpServer=0.pool.ntp.org \
-persist.dirac.acs.controller=afm \
-persist.dirac.acs.storeSettings=1 \
-persist.dirac.afm.mode=global \
-persist.dirac.poolsize=3 \
-persist.fuse_sdcard=true \
-persist.mm.sta.enable=0 \
-persist.vendor.audio.speaker.prot.enable=false \
-persist.vendor.data.profile_update=true \
-persist.vendor.radio.prefer_spn=1 \
-ro.dalvik.vm.native.bridge=0 \
-ro.memperf.enable=false \
-ro.vendor.display.sensortype=2 \
-ro.vendor.qti.am.reschedule_service=true \
-ro.vendor.qti.sys.fw.bservice_age=5000 \
-ro.vendor.qti.sys.fw.bservice_limit=5 \
-ro.vendor.qti.sys.fw.empty_app_percent=50 \
-ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-ro.vendor.qti.sys.fw.trim_empty_percent=100 \
-ro.vendor.qti.sys.fw.trim_enable_memory=2147483648 \
-ro.vendor.qti.sys.fw.use_trim_settings=true \
-sys.vendor.shutdown.waittime=500 \
-vendor.audio.dolby.ds2.enabled=false \
-vendor.audio.dolby.ds2.hardbypass=false \
-vendor.audio.offload.passthrough=false
